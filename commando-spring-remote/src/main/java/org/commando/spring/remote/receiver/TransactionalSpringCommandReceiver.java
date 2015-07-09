fac5e5e0f9fcfbdfdd865d788c0528339ef041a9
pborbas <peetair@gmail.com>
2015-07-09 12:56:45 +0200

    transactional spring receiver added

---
+++ commando-spring-remote/src/main/java/org/commando/spring/remote/receiver/TransactionalSpringCommandReceiver.java
@@ -0,0 +1,24 @@
+package org.commando.spring.remote.receiver;
+
+import org.commando.dispatcher.ChainableDispatcher;
+import org.commando.remote.model.TextDispatcherCommand;
+import org.commando.remote.model.TextDispatcherResult;
+import org.commando.remote.receiver.DefaultCommandReceiver;
+import org.commando.remote.serializer.Serializer;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by pborbas on 26/06/15.
+ */
+public class TransactionalSpringCommandReceiver extends DefaultCommandReceiver {
+
+	public TransactionalSpringCommandReceiver(Serializer serializer, ChainableDispatcher dispatcher) {
+		super(serializer, dispatcher);
+	}
+
+	@Override
+	@Transactional
+	public TextDispatcherResult execute(TextDispatcherCommand textDispatcherCommand) {
+		return super.execute(textDispatcherCommand);
+	}
+}
